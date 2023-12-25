658b4c4ef19c88d91ba0d3a47ab89cd856d9fb89
Michael <yanghao13111@gmail.com>
2023-12-25 18:40:07 +0800

    all function

--- public/script.js
+++ public/script.js
@@ -1,72 +1,277 @@
-// 建立WebSocket連接
-const ws = new WebSocket('ws://localhost:8081');
+// 连接到Socket.IO服务器
+const socket = io();
 
-ws.onopen = function() {
-    console.log('Connected to the WebSocket server');
-};
-
-ws.onmessage = function(event) {
-    const [action, result] = event.data.split('|');
-    switch(action) {
-        case 'LOGIN_RESPONSE':
-            if (result === 'SUCCESS') {
-                window.location.href = '/profile.html';
-            } else {
-                alert('登錄失敗，請檢查您的帳號或密碼。');
-            }
-            break;
-        case 'REGISTER_RESPONSE':
-            if (result === 'SUCCESS') {
-                alert('註冊成功，請登錄。');
-                window.location.href = '/';
-            } else {
-                alert('註冊失敗，用戶名可能已存在。');
-            }
-            break;
-    }
-};
-
-function login(username, password) {
-    ws.send(`LOGIN|${username}|${password}`);
-}
-
-function register(username, password) {
-    ws.send(`REGISTER|${username}|${password}`);
-}
-
-function logout() {
-    ws.send('LOGOUT');
-    localStorage.removeItem('username'); // 假設您在localStorage中儲存了用戶名
-    window.location.href = '/';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('login-form');
-    const registerForm = document.getElementById('register-form');
-    const logoutButton = document.getElementById('logout');
-
-    if (loginForm) {
-        loginForm.onsubmit = function(e) {
-            e.preventDefault();
-            const username = document.getElementById('login-username').value;
-            const password = document.getElementById('login-password').value;
-            login(username, password);
-        };
-    }
-
-    if (registerForm) {
-        registerForm.onsubmit = function(e) {
-            e.preventDefault();
-            const username = document.getElementById('register-username').value;
-            const password = document.getElementById('register-password').value;
-            register(username, password);
-        };
-    }
-
-    if (logoutButton) {
-        logoutButton.onclick = function(e) {
-            e.preventDefault();
-            logout();
-        };
-    }
+socket.on('connect', () => {
+  console.log('Connected to the server.');
 });
+
+// 登入功能
+function login() {
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+  
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    }).then(response => {
+      if (response.ok) return response.json();
+      throw new Error('Login failed.');
+    }).then(data => {
+      localStorage.setItem('username', data.username); // 存储用户名
+      window.location.href = '/profile.html';
+    }).catch(error => {
+      alert(error);
+    });
+  }
+  
+  // 註冊功能
+  function register() {
+    const username = document.getElementById('registerUsername').value;
+    const password = document.getElementById('registerPassword').value;
+  
+    fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    }).then(response => {
+      if (response.ok) {
+        alert('Registration successful!');
+        window.location.href = '/index.html';
+      } else {
+        alert('Registration failed.');
+      }
+    }).catch(error => {
+      alert(error);
+    });
+  }
+  
+  // 登出功能
+  function logout() {
+    fetch('/logout', {
+      method: 'POST',
+    }).then(() => {
+      window.location.href = '/index.html';
+    });
+  }
+
+
+// 获取留言并显示（使用Socket.IO）
+function loadMessages() {
+  socket.on('loadMessages', (messages) => {
+    const messagesDiv = document.getElementById('messages');
+    messagesDiv.innerHTML = messages.map(msg => 
+      `<div class="message-container">
+          <div class="message-header">${msg.subject}</div>
+          <div>${msg.content}</div>
+          <div class="message-footer">${msg.author} - ${new Date(msg.createdAt).toLocaleString()}</div>
+      </div>`
+    ).join('');
+  });
+}
+
+// 发布留言（使用Socket.IO）
+function postMessage() {
+  const subject = document.getElementById('subject').value;
+  const content = document.getElementById('content').value;
+  console.log('Sending message:', { subject, content});
+  // 发送留言到服务器
+  socket.emit('postMessage', { subject, content});
+  // 监听服务器确认消息已保存
+  socket.on('messagePosted', () => {
+    window.location.href = '/messageboard.html'; // 跳转到留言板页面
+  });
+}
+
+// 监听来自服务器的新留言
+socket.on('newMessage', (msg) => {
+  // 将新留言添加到留言板
+  addMessageToBoard(msg);
+});
+
+// 获取留言并显示
+function loadMessages() {
+  // 请求服务器获取所有留言
+  socket.emit('requestMessages');
+
+  // 接收服务器发送的所有留言
+  socket.on('loadMessages', (messages) => {
+    const messagesDiv = document.getElementById('messages');
+    messagesDiv.innerHTML = messages.map(msg => 
+      `<div class="message-container">
+          <div class="message-header">${msg.subject}</div>
+          <div>${msg.content}</div>
+          <div class="message-footer">${msg.author} - ${new Date(msg.createdAt).toLocaleString()}</div>
+      </div>`
+    ).join('');
+  });
+}
+
+// 页面加载时获取留言
+if (window.location.pathname === '/messageboard.html') {
+    loadMessages();
+}
+
+// 網路視訊
+
+// Global variables
+let localStream = null;
+let remoteStream = null;
+let peerConnection = null;
+const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
+
+socket.on('online-users', (users) => {
+  // 更新UI以显示在线用户
+  const usersList = document.getElementById('onlineUsers');
+  usersList.innerHTML = '';
+  users.forEach(user => {
+      if (user !== localStorage.getItem('username')) {  // 不显示自己
+          usersList.innerHTML += `<li><button onclick="initiateCall('${user}')">${user}</button></li>`;
+      }
+  });
+});
+
+function initiateCall(otherUsername) {
+  if (otherUsername) {
+      console.log(`Initiating call with ${otherUsername}`);
+      // 发送呼叫请求给服务器，服务器需要转发给特定的用户
+      socket.emit('initiate-call', { to: otherUsername });
+  }
+}
+
+function toggleCamera() {
+  if (localStream) {
+    const videoTrack = localStream.getVideoTracks()[0];
+    videoTrack.enabled = !videoTrack.enabled;
+  }
+}
+
+function toggleMicrophone() {
+  if (localStream) {
+    const audioTrack = localStream.getAudioTracks()[0];
+    audioTrack.enabled = !audioTrack.enabled;
+  }
+}
+
+// 获取本地视频和音频
+function startVideo() {
+  navigator.mediaDevices.getUserMedia({audio: true, video: true})
+    .then(stream => {
+      localStream = stream;
+      document.getElementById('localVideo').srcObject = localStream;
+    })
+    .catch(error => {
+      console.error('Error opening video camera.', error);
+    });
+}
+
+// 创建WebRTC连接
+function createPeerConnection() {
+  peerConnection = new RTCPeerConnection(configuration);
+
+  // 当找到ICE候选时发送给远端
+  peerConnection.onicecandidate = ({candidate}) => {
+    if(candidate) {
+      socket.emit('new-ice-candidate', {candidate});
+    }
+  };
+
+  // 当远端流到达时，将其设置到远端视频对象中
+  peerConnection.ontrack = (event) => {
+    if (!remoteStream) {
+      remoteStream = new MediaStream();
+      document.getElementById('remoteVideo').srcObject = remoteStream;
+    }
+    remoteStream.addTrack(event.track);
+  };
+
+  // 添加本地流到连接
+  localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+  });
+}
+
+// 呼叫其他用户
+function call() {
+  createPeerConnection();
+  peerConnection.createOffer()
+    .then(offer => peerConnection.setLocalDescription(offer))
+    .then(() => {
+      socket.emit('video-offer', {offer: peerConnection.localDescription});
+    })
+    .catch(error => console.error('Error creating offer: ', error));
+}
+
+// 接受呼叫
+function handleOffer(offer) {
+  if (!peerConnection) createPeerConnection();
+  peerConnection.setRemoteDescription(new RTCSessionDescription(offer))
+    .then(() => peerConnection.createAnswer())
+    .then(answer => peerConnection.setLocalDescription(answer))
+    .then(() => {
+      socket.emit('video-answer', {answer: peerConnection.localDescription});
+    })
+    .catch(error => console.error('Error handling offer: ', error));
+}
+
+// 处理远端的回应
+function handleAnswer(answer) {
+  peerConnection.setRemoteDescription(new RTCSessionDescription(answer))
+    .catch(error => console.error('Error setting remote description: ', error));
+}
+
+// 处理新的ICE候选
+function handleNewICECandidate(candidate) {
+  peerConnection.addIceCandidate(candidate)
+    .catch(error => console.error('Error adding received ice candidate', error));
+}
+
+// 退出视频通话
+function hangupVideo() {
+  if (peerConnection) {
+    peerConnection.close();
+    peerConnection = null;
+  }
+  if (localStream) {
+    localStream.getTracks().forEach(track => track.stop());
+    localStream = null;
+  }
+  remoteStream = null;
+  document.getElementById('localVideo').srcObject = null;
+  document.getElementById('remoteVideo').srcObject = null;
+  window.location.href = '/profile.html'; // 跳转回个人主页
+}
+
+// 监听Socket.IO事件
+socket.on('video-offer', data => {
+  handleOffer(data.offer);
+});
+
+socket.on('video-answer', data => {
+  handleAnswer(data.answer);
+});
+
+socket.on('new-ice-candidate', data => {
+  handleNewICECandidate(data.candidate);
+});
+
+socket.on('user-disconnected', () => {
+  console.log('The other user has disconnected.');
+  hangupVideo();  // 调用挂断视频的函数
+});
+
+socket.on('incoming-call', (data) => {
+  if (confirm(`Incoming call from ${data.from}, accept?`)) {
+      // 接受呼叫逻辑
+      call();
+  }
+});
+
+socket.on('call-error', (data) => {
+  alert(data.message); // 显示错误信息，例如对方不在线
+});
+
--- src/server.js
+++ src/server.js
@@ -1,80 +1,145 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const { MongoClient } = require('mongodb');
-const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
+const onlineUsers = {};
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 
-// MongoDB connection settings
-const uri = "mongodb://localhost:27017/";
-const client = new MongoClient(uri);
-let usersCollection;
+// 连接到MongoDB
+mongoose.connect('mongodb://localhost:27017/appDatabase');
 
-async function main() {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB");
+// 定义用户模型
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model('User', userSchema);
 
-        const db = client.db("appDatabase");
-        usersCollection = db.collection("users");
+// 定义留言模型
+const messageSchema = new mongoose.Schema({
+  subject: String,
+  content: String,
+  author: String,
+  createdAt: { type: Date, default: Date.now },
+});
+const Message = mongoose.model('Message', messageSchema);
 
-        app.listen(PORT, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        });
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit(1);
-    }
-}
-
-main();
-
-// Register endpoint
+// 注册新用户
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await usersCollection.findOne({ username });
-        if (user) {
-            return res.status(400).json({ success: false, message: 'Username already exists' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await usersCollection.insertOne({ username, password: hashedPassword });
-        res.json({ success: true, message: 'User registered successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Server error' });
-    }
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).send('User created');
+  } catch (error) {
+    res.status(500).send('Error registering new user: ' + error.message);
+  }
 });
 
-// Login endpoint
+// 用户登录
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await usersCollection.findOne({ username });
-        if (!user) {
-            return res.status(401).json({ success: false, message: 'Incorrect username or password' });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (match) {
-            res.cookie('username', username, { httpOnly: true });
-            res.json({ success: true, message: 'Logged in successfully' });
-        } else {
-            res.status(401).json({ success: false, message: 'Incorrect username or password' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Server error' });
-    }
+  const user = await User.findOne({ username: req.body.username });
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    res.cookie('username', user.username, { httpOnly: true, maxAge: 900000 }); // maxAge in milliseconds
+    res.status(200).json({ message: 'Logged in' });
+  } else {
+    res.status(400).send('Not Authorized');
+  }
 });
 
-// Logout endpoint
+// 用户登出
 app.post('/logout', (req, res) => {
-    res.clearCookie('username');
-    res.json({ success: true, message: 'Logged out successfully' });
+  res.clearCookie('username');
+  res.send('Logged out');
+});
+
+// Socket.IO通信
+io.on('connection', (socket) => {
+
+  // 当用户登录时
+  socket.on('user-login', (username) => {
+      onlineUsers[socket.id] = username;
+      io.emit('online-users', Object.values(onlineUsers));
+  });
+
+  // 当用户断开连接时
+  socket.on('disconnect', () => {
+      delete onlineUsers[socket.id];
+      io.emit('online-users', Object.values(onlineUsers));
+  });
+
+
+  // 发布留言
+  socket.on('postMessage', async (msg) => {
+    console.log('Received message:', msg);
+    console.log('A user connected with username:', socket.handshake.query.username);
+    try {
+      const usernameFromCookie = socket.handshake.query.username;
+      const newMessage = new Message({
+        subject: msg.subject,
+        content: msg.content,
+        author: usernameFromCookie || 'Anonymous',
+      });
+      const savedMessage = await newMessage.save();
+      io.emit('newMessage', savedMessage); // Broadcast the new message to all clients
+      socket.emit('messagePosted'); // 告诉发送消息的客户端消息已发布
+    } catch (error) {
+      console.error('Error saving message:', error);
+    }
+  });
+
+  // 监听来自客户端的请求消息事件
+  socket.on('requestMessages', async () => {
+    try {
+      const messages = await Message.find().sort({ createdAt: -1 });
+      socket.emit('loadMessages', messages);  // 发送所有留言给请求的客户端
+    } catch (error) {
+      console.error('Error retrieving messages:', error);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+    socket.broadcast.emit('user-disconnected');
+  });
+
+  socket.on('initiate-call', (data) => {
+    const recipientSocketId = Object.keys(onlineUsers).find(key => onlineUsers[key] === data.to);
+    if (recipientSocketId) {
+        io.to(recipientSocketId).emit('incoming-call', { from: onlineUsers[socket.id] });
+    } else {
+        socket.emit('call-error', { message: `${data.to} is not online.` });
+    }
+  });
+
+  // 处理视频通话信令
+  socket.on('video-offer', (data) => {
+    socket.broadcast.emit('video-offer', { offer: data.offer });
+  });
+
+  socket.on('video-answer', (data) => {
+      socket.broadcast.emit('video-answer', { answer: data.answer });
+  });
+
+  socket.on('new-ice-candidate', (data) => {
+      socket.broadcast.emit('new-ice-candidate', { candidate: data.candidate });
+  });
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
